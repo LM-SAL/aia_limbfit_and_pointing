95f86cf5888981f74fdb73c593d986a7d043e04b
John Serafin <jps@sun.stanford.edu>
2016-11-09 11:02:04 -0800

    Add general and specific filtering.
    New $filt option for query string $qs. Require IMG_TYPE = DARK
    and ACS_MODE = SCIENCE.

--- get_paths.pl
+++ get_paths.pl
@@ -7,11 +7,13 @@ use Getopt::Long;
 $ENV{SUMSERVER} = "k1";
 $ENV{SGE_ROOT} = "/SGE";
 my $JSOC_MACHINE = $ENV{JSOC_MACHINE};
-my ($yr, $mon, $hr, $da, $month, $year, $outnam, $qs, $wl, $stage);
+my ($yr, $mon, $hr, $da, $month, $year, $outnam, $qs, $wl, $stage, @lines);
 my $show_info = "/home/jsoc/cvs/Development/JSOC/bin/$JSOC_MACHINE/show_info";
 my $path_dir = '/tmp28/jps/LimbFit/paths';
 my $stage_dir = '/tmp28/jps/LimbFit/stage';
+my $filt = '';
 my $dur = '3h';
+my $kw = 'IMG_TYPE,ACS_MODE';
 my $t0 = time;
 ($hr, $da, $month, $year) = (gmtime($t0))[2, 3, 4, 5];
 $yr = $year + 1900; $mon = $month + 1; $hr = 3*int($hr/3);
@@ -21,6 +23,7 @@ GetOptions(
            "day=i" => \$da,
            "hour=i" => \$hr,
            "dur=s" => \$dur,
+           "filt=s" => \$filt,
            "stage!" => \$stage
           );
 
@@ -29,20 +32,34 @@ my $outdir = sprintf "$path_dir/$yr/%2.2d/%2.2d", $mon, $da;
 for $wl (94, 131, 171, 193, 211, 304, 335) {
   $outnam = sprintf "$outdir/$yr%2.2d%2.2d_%2.2d_%4.4d.images",
                $mon, $da, $hr, $wl;
-  $qs = "aia.lev1_euv_12s[$yr.$mon.${da}_$hr/$dur\@2m][$wl]";
-  `$show_info -q -P seg=image $qs > $outnam`;
-   copy $outnam, $stage_dir if $stage;
+  $qs = "aia.lev1_euv_12s[$yr.$mon.${da}_$hr/$dur\@2m][$wl]$filt";
+  @lines = `$show_info -q -P key=$kw seg=image $qs`;
+  save_paths($outnam, @lines);
+  copy $outnam, $stage_dir if $stage;
 }
 for $wl (1600, 1700) {
   $outnam = sprintf "$outdir/$yr%2.2d%2.2d_%2.2d_%4.4d.images",
                $mon, $da, $hr, $wl;
-  $qs = "aia.lev1_uv_24s[$yr.$mon.${da}_$hr/$dur\@2m][$wl]";
-  `$show_info -q -P seg=image $qs > $outnam`;
-   copy $outnam, $stage_dir if $stage;
+  $qs = "aia.lev1_uv_24s[$yr.$mon.${da}_$hr/$dur\@2m][$wl]$filt";
+  @lines = `$show_info -q -P key=$kw seg=image $qs`;
+  save_paths($outnam, @lines);
+  copy $outnam, $stage_dir if $stage;
 }
 $wl = 4500;
 $outnam = sprintf "$outdir/$yr%2.2d%2.2d_%2.2d_%4.4d.images",
              $mon, $da, $hr, $wl;
-$qs = "aia.lev1_vis_1h[$yr.$mon.${da}_$hr/$dur][$wl]";
-`$show_info -q -P seg=image $qs > $outnam`;
+$qs = "aia.lev1_vis_1h[$yr.$mon.${da}_$hr/$dur][$wl]$filt";
+@lines = `$show_info -q -P key=$kw seg=image $qs`;
+save_paths($outnam, @lines);
 copy $outnam, $stage_dir if $stage;
+
+sub save_paths
+{
+  my ($out, @lines) = @_;
+  open(my $fhout, ">", $out) or die "$0: can't open $out for writing: $!";
+  for (@lines) {
+    my ($type, $mode, $path) = split;
+    print $fhout "$path\n" if $type =~ /LIGHT/ and $mode =~ /SCIENCE/;
+  }
+  close $fhout;
+}
