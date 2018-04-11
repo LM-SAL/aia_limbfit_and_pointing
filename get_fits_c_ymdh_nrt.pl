d97938e93029024ddb6152190b69ec5ecd2b7d53
John Serafin <jps@sun.stanford.edu>
2018-04-11 14:01:51 -0700

    Initial get_fits_c_ymdh_nrt

---
+++ get_fits_c_ymdh_nrt.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+$ENV{SUMSERVER} = "k1";
+$ENV{SGE_ROOT} = "/SGE";
+my $JSOC_MACHINE = $ENV{JSOC_MACHINE};
+my ($yr, $mo, $hr, $da, $dur, $inpnam, $outnam, $qs, @files, $n, $wl, $sum);
+my $outroot = '/tmp29/jps/LimbFit_c/fits_nrt';
+
+GetOptions(
+           "outroot=s" => \$outroot,
+           "year=i" => \$yr,
+           "month=i" => \$mo,
+           "day=i" => \$da,
+           "hour=i" => \$hr,
+           "dur=s" => \$dur
+          );
+my $outdir = sprintf "$outroot/$yr/%.2d/%.2d", $mo, $da;
+`mkdir -p $outdir` unless -e $outdir;
+my $series = 'aia.lev1_nrt2';
+for my $w (94, 131, 171, 193, 211, 304, 335, 1600, 1700, 4500) {
+  $outnam = sprintf "$yr%.2d%.2d_%.2d_%.4d.limb", $mo, $da, $hr, $w;
+  $qs = sprintf "$series\[$yr.%.2d.%.2d_%.2d/$dur]\[?WAVELNTH=$w?]", $mo, $da, $hr;
+  $sum = 1;
+  $sum = 3 if $w < 4000;
+  $sum = 5 if $w < 1500;
+  system "/home/jps/bin/limbfit_aia dsinp=$qs sum=$sum > $outdir/$outnam";
+}
